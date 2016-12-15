63ad44e15f7a3f6a28a766365239eb0535b7922e
Holger Kruse <mail2holger@gmail.com>
2016-12-15 13:20:09 +0100

    tests

---
+++ test/settype.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""
+set atom type for water boxes
+"""
+
+
+r=open('30box.xyz','r')
+w=open('30box.xyzt','w')
+
+for line in range(0,2):
+ newline=r.readline()
+ w.write(newline)
+
+
+# TIP3P names
+for line in r.readlines():
+ if "o" in line:
+  newline=line.rstrip()+' OW\n'
+  w.write(newline)
+ if "h" in line:
+  newline=line.rstrip()+' HW\n'
+  w.write(newline)
