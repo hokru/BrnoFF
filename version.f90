a89c7d25d8f1abc94ce07217f196fbd7a7a50529
Holger Kruse <mail2holger@gmail.com>
2019-09-16 10:21:03 +0200

    ungit version.f90

--- version.f90
+++
@@ -1,6 +0,0 @@
-subroutine version
- print*,  'Build info:'
- print*,  ' build date    : Mon Sep 16 10:08:45 CEST 2019     '
- print*,  ' git version   : 9624-dirty'
- print*,  ' '          
-end subroutine
